b1a1d7574a9d51b079358b238f2037f655b87640
mplummer123 <mplummer3011@gmail.com>
2016-11-14 14:00:40 +0000

    Added lectures 7 and 8 for torb and ian

--- Coursework/Ian/CourseWork2/Part1LinearRegression.m
+++ Coursework/Ian/CourseWork2/Part1LinearRegression.m
@@ -1,6 +1,8 @@
-%%
+%Generate 100 sample points using the equation for a 1D line: y = mx + C
+% Use the following parameters: x-axis starting point = -4; x-axis ending point = 6;
+% m = 1.6;
+% C = 6;
     b0 = 3;
     b1 = 4;
     f = @(x) b0-b1*x;
-    %%
-    ezplot( f, 0, 5 )
+    ezplot(f,0,5)
