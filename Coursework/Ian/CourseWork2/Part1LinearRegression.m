ed26c05f8a7fc778ddbeb78918678c8b122888ef
mplummer123 <mplummer3011@gmail.com>
2016-11-20 16:59:38 +0000

    Completed task 1 of Ians second coursework piece

--- Coursework/Ian/CourseWork2/Part1LinearRegression.m
+++ Coursework/Ian/CourseWork2/Part1LinearRegression.m
@@ -1,8 +1,27 @@
-%Generate 100 sample points using the equation for a 1D line: y = mx + C
-% Use the following parameters: x-axis starting point = -4; x-axis ending point = 6;
-% m = 1.6;
-% C = 6;
-    b0 = 3;
-    b1 = 4;
-    f = @(x) b0-b1*x;
-    ezplot(f,0,5)
+%Specify number of samples to take
+samples = 100;
+
+%Set limits for x value
+minX = -4;
+maxX = 6;
+
+%Sample x and specify other variable
+x = linspace(-4,6);
+c = 6;
+m = 1.6;
+
+%Combine variables to calculate y in a linear equation
+y = m*x+c;
+
+%Create noise for data in a Gaussian format (default std = 1 mean = 0)
+r = randn(1,samples);
+
+%Plot x against y adding on the Gaussian noise 
+figure
+plot(x,y+r,'-o','linewidth',2) 
+ylabel('Data Point Value');
+xlabel('Data Point Number');
+title('dataNormal raw data plot');
+legend('rawData');
+%plot(x,r(1,:)+y,'-o','linewidth',2) 
+
