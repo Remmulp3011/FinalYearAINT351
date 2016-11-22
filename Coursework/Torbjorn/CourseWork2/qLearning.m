409788b5709c55ad124779477bc3be388190a5cf
mplummer123 <mplummer3011@gmail.com>
2016-11-22 15:37:03 +0000

    Adding comments for what was discussed to the q-learning function on what to do next.

---
+++ Coursework/Torbjorn/CourseWork2/qLearning.m
@@ -0,0 +1,16 @@
+function qLearning(qTable,state,action,nextState,reward)
+%QLEARNING Summary of this function goes here
+%   Detailed explanation goes here
+
+%insert the values into the equation so with each iteration you are
+%updating the values of random q-table. s' being the next state, a' being
+%the next action, s being the intial state and a being the intial action.
+%Goal is to update the values based on the rewards so the closer to the
+%goal you get the closer to 10 you get. you update the cell of s' and a'
+%with the new value. To start it will be shuffling around the values but
+%with each iteration you start to back propagate through updating the cells
+%to get closer to the reward. Goal is to find a' with the max function
+%getting the max value action for the given state. To start reward value
+%will be 10 or 0 (based on the reward function).
+end
+
