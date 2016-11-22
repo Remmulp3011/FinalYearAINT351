e26d53fe99011ee1398df8aba4a7d10407378a27
mplummer123 <mplummer3011@gmail.com>
2016-11-22 15:17:44 +0000

    Added comments on what to do next for the decision tree coursework and finished the greedy function and fixed errors

--- Coursework/Torbjorn/CourseWork2/eGreedyActionSelection.m
+++ Coursework/Torbjorn/CourseWork2/eGreedyActionSelection.m
@@ -1,7 +1,28 @@
 function eGreedyActionSelection(qTable,state)
 %EGREEDYACTIONSELECTION Summary of this function goes here
 %   Detailed explanation goes here
+action = 1;
+maxActionValue = 0;
+maxAction = zeros(1,1);
+randomNum = rand(1,1);
 
+for n=1:size(qTable,2)
+    value = qTable(state,action);
+        if(value>maxActionValue)
+            maxActionValue = value;
+            maxAction(1,1) = action;
+        end
+    action = action+1;
+end
+
+if(randomNum <= 0.9)
+    disp('max action is:');
+    disp(maxAction);
+else
+    disp('Random action is:')
+    randomAction = randi(length(qTable(2)));
+    disp(randomAction);
+end
 
 end
 
--- Coursework/Torbjorn/CourseWork2/initQ.m
+++ Coursework/Torbjorn/CourseWork2/initQ.m
@@ -13,6 +13,7 @@ zlabel('Q-values')
 %axis = [xmin,xmax,ymin,ymax,zmin,zmax]
 axis([1,4,1,11,0,1])
 
+disp(qTable);
 eGreedyActionSelection(qTable,1);
 
 end
